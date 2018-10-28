1721b1edea6bcdeecf510dff38538cdd7f960db9
Steven <steven.mucian@gmail.com>
2018-10-29 01:40:53 +0700

    add product module

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -17,11 +17,37 @@ class Product_model extends STEVEN_Model
         $this->column_order     = array("$this->table.id", "$this->table.id", "$this->table_trans.title", "$this->table.is_featured", "$this->table.is_status", "$this->table.created_time", "$this->table.updated_time");
         $this->column_search    = array("$this->table_trans.title");
         $this->order_default    = array("$this->table.id" => "ASC");
-
     }
     public function _where_custom($args = array()){
         parent::_where_custom();
         extract($args);
 
+        if(!empty($category_id)){
+            $this->db->join($this->table_category,"$this->table.id = $this->table_category.{$this->table}_id");
+            $this->db->where_in("$this->table_category.category_id",$category_id);
+        }
+
+    }
+
+    public function getCategoryByProductId($id, $lang_code = null){
+        if(empty($lang_code)) $lang_code = $this->session->userdata('admin_lang') ? $this->session->userdata('admin_lang') : $this->session->userdata('public_lang_code');
+        $this->db->select();
+        $this->db->from($this->table_category);
+        $this->db->join("category_translations","$this->table_category.category_id = category_translations.id");
+        $this->db->join("category","$this->table_category.category_id = category.id");
+        $this->db->where('category_translations.language_code', $lang_code);
+        $this->db->where($this->table_category.".product_id", $id);
+        $data = $this->db->get()->result();
+        return $data;
+    }
+    public function getSelect2Category($id, $lang_code = null){
+        if(empty($lang_code)) $lang_code = $this->session->userdata('admin_lang') ? $this->session->userdata('admin_lang') : $this->session->userdata('public_lang_code');
+        $this->db->select("$this->table_category.category_id AS id, category_translations.title AS text");
+        $this->db->from($this->table_category);
+        $this->db->join("category_translations","$this->table_category.category_id = category_translations.id");
+        $this->db->where('category_translations.language_code', $lang_code);
+        $this->db->where($this->table_category.".product_id", $id);
+        $data = $this->db->get()->result();
+        return $data;
     }
 }
---
+++ public/backend/js/page/product.js
@@ -0,0 +1,155 @@
+// Dom Ready
+$(function() {
+    datatables_columns = [{
+        field: "checkID",
+        title: "#",
+        width: 50,
+        sortable: !1,
+        textAlign: "center",
+        selector: {class: "m-checkbox--solid m-checkbox--brand"}
+    },{
+        field: "id",
+        title: "ID",
+        width: 50,
+        textAlign: "center",
+        sortable: 'asc',
+        filterable: !1,
+    }, {
+        field: "title",
+        title: "Tiêu đề",
+        width: 300
+    }, {
+        field: "is_featured",
+        title: "Nổi bật",
+        width: 70,
+        textAlign: "center",
+        template: function (t) {
+            return '<span data-field="is_featured" data-value="'+(t.is_featured == 1 ? 0 : 1)+'" class="btnUpdateField">' + (t.is_featured == 1 ? '<i class="la la-star"></i>' : '<i class="la la-star-o"></i>') + "</span>"
+        }
+    }, {
+        field: "is_status",
+        title: "Status",
+        textAlign: "center",
+        width: 70,
+        template: function (t) {
+            var e = {
+                0: {title: "Disable", class: "m-badge--danger"},
+                1: {title: "Active", class: "m-badge--primary"},
+            };
+            return '<span data-field="is_status" data-value="'+(t.is_status == 1 ? 0 : 1)+'" class="m-badge ' + e[t.is_status].class + ' m-badge--wide btnUpdateField">' + e[t.is_status].title + "</span>"
+        }
+    }, {
+        field: "updated_time",
+        title: "Updated Time",
+        type: "date",
+        textAlign: "center",
+        format: "MM/DD/YYYY"
+    }, {
+        field: "created_time",
+        title: "Created Time",
+        type: "date",
+        textAlign: "center",
+        format: "MM/DD/YYYY"
+    }, {
+        field: "action",
+        width: 110,
+        title: "Actions",
+        sortable: !1,
+        overflow: "visible",
+        template: function (t, e, a) {
+            return '' +
+                '<a href="javascript:;" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill btnEdit" title="Edit"><i class="la la-edit"></i></a>' +
+                '<a href="javascript:;" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill btnDelete" title="Delete"><i class="la la-trash"></i></a>'
+        }
+    }];
+    AJAX_DATATABLES.init();
+    loadCategory();
+    AJAX_CRUD_MODAL.init();
+    AJAX_CRUD_MODAL.tinymce();
+    SEO.init_slug();
+
+    $('[name="is_status"]').on("change", function () {
+        table.search($(this).val(), "is_status")
+    }), $('[name="is_status"]').selectpicker();
+
+    $('select[name="category_id"]').on("change", function () {
+        table.search($(this).val(), "category_id")
+    });
+
+    $('#modal_form').on('shown.bs.modal', function(e){
+        loadCategory();
+    });
+
+    $(document).on('click','.btnEdit',function () {
+        let modal_form = $('#modal_form');
+        let id = $(this).closest('tr').find('input[type="checkbox"]').val();
+        AJAX_CRUD_MODAL.edit(function () {
+            $.ajax({
+                url : url_ajax_edit,
+                type: "POST",
+                data: {id:id},
+                dataType: "JSON",
+                success: function(response) {
+                    $.each(response.data_info, function( key, value ) {
+                        let element = modal_form.find('[name="'+key+'"]');
+                        element.val(value);
+                        if(element.hasClass('switchBootstrap')){
+                            element.bootstrapSwitch('state',(value == 1 ? true : false));
+                        }
+                    });
+
+                    $.each(response.data_language, function( i, value ) {
+                        let lang_code = value.language_code;
+                        $.each(value, function( key, val) {
+                            let element = modal_form.find('[name="language['+lang_code+']['+key+']"]');
+                            if(element.hasClass('tinymce') && val){
+                                tinymce.get(element.attr('id')).setContent(val);
+                            }
+                            element.val(val);
+                        });
+                    });
+
+                    loadCategory(response.data_category);
+                    modal_form.modal('show');
+                },
+                error: function (jqXHR, textStatus, errorThrown)
+                {
+                    console.log(errorThrown);
+                    console.log(textStatus);
+                    console.log(jqXHR);
+                }
+            });return false;
+        });
+    });
+});
+
+function loadCategory(dataSelected) {
+    let selector = $('select.category');
+    selector.select2({
+        placeholder: 'Chọn danh mục',
+        allowClear: !0,
+        multiple: !1,
+        data: dataSelected,
+        ajax: {
+            url: url_ajax_load_category,
+            dataType: 'json',
+            delay: 250,
+            data: function(e) {
+                return {
+                    q: e.term,
+                    page: e.page
+                }
+            },
+            processResults: function(e, t) {
+                return t.page = t.page || 1, {
+                    results: e,
+                    pagination: {
+                        more: 30 * t.page < e.total_count
+                    }
+                }
+            },
+            cache: !0
+        }
+    });
+    if (typeof dataSelected !== 'undefined') selector.find('> option').prop("selected", "selected").trigger("change");
+}
