bd819b65c2505e0fecc9f5e00ed50d16ec2f6824
Steven <steven.mucian@gmail.com>
2018-11-10 17:49:06 +0700

    update function timeAgo

--- application/helpers/datetime_helper.php
+++ application/helpers/datetime_helper.php
@@ -1,67 +1,31 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 if (!function_exists('timeAgo')) {
-    function timeAgo($datetime, $format = false) {
-        if(strtotime($datetime) < 0) return $datetime = date('d/m/Y',strtotime("+3 month"));
-        if($format !== false) return date($format,strtotime($datetime));
-        $today = time();
-        if(!is_numeric($datetime)) $createdday= strtotime($datetime);
-        else $createdday = $datetime;
-        $datediff = abs($today - $createdday);
-        $difftext="";
-        $years = floor($datediff / (365*60*60*24));
-        $months = floor(($datediff - $years * 365*60*60*24) / (30*60*60*24));
-        $days = floor(($datediff - $years * 365*60*60*24 - $months*30*60*60*24)/ (60*60*24));
-        $hours= floor($datediff/3600);
-        $minutes= floor($datediff/60);
-        $seconds= floor($datediff);
-        //năm checker
-        if($difftext=="")
-        {
-            if($years>1)
-                $difftext=$years." năm trước";
-            elseif($years==1)
-                $difftext=$years." năm trước";
+    function timeAgo($datetime, $full = false) {
+        $now = new DateTime();
+        $ago = new DateTime($datetime);
+        $diff = $now->diff($ago);
+        $diff->w = floor($diff->d / 7);
+        $diff->d -= $diff->w * 7;
+
+        $string = array(
+            'y' => 'năm',
+            'm' => 'tháng',
+            'w' => 'tuần',
+            'd' => 'ngày',
+            'h' => 'giờ',
+            'i' => 'phút',
+            's' => 'giây',
+        );
+        foreach ($string as $k => &$v) {
+            if ($diff->$k) {
+                $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+            } else {
+                unset($string[$k]);
+            }
         }
-        //month checker
-        if($difftext=="")
-        {
-            if($months>1)
-                $difftext=$months." tháng trước";
-            elseif($months==1)
-                $difftext=$months." tháng trước";
-        }
-        //month checker
-        if($difftext=="")
-        {
-            if($days>1)
-                $difftext=$days." ngày trước";
-            elseif($days==1)
-                $difftext=$days." ngày trước";
-        }
-        //hour checker
-        if($difftext=="")
-        {
-            if($hours>1)
-                $difftext=$hours." giờ trước";
-            elseif($hours==1)
-                $difftext=$hours." giờ trước";
-        }
-        //minutes checker
-        if($difftext=="")
-        {
-            if($minutes>1)
-                $difftext=$minutes." phút trước";
-            elseif($minutes==1)
-                $difftext=$minutes." phút trước";
-        }
-        //seconds checker
-        if($difftext=="")
-        {
-            if($seconds>1)
-                $difftext=$seconds." giây trước";
-            elseif($seconds==1)
-                $difftext=$seconds." giây trước";
-        }
-        return $difftext;
+
+        if (!$full) $string = array_slice($string, 0, 1);
+        else return date($full, strtotime($datetime));
+        return $string ? implode(', ', $string) . ' trước' : 'vừa xong';
     }
 }
