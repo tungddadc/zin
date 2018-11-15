f14fe714a68180b579827d8d195401f7b7d2f4f9
Steven <steven.mucian@gmail.com>
2018-11-16 02:46:51 +0700

    fix ajax search autocomplete

--- public/js/custom.js
+++ public/js/custom.js
@@ -600,6 +600,7 @@ jQuery(document).ready(function () {
     $(document).on('keyup','input#search',function () {
         let element = $(this);
         let keyword = $(this).val();
+        element.closest('form').find('.product_search').html('').removeClass('go_in')
         setTimeout(function () {
             if(keyword){
                 $.ajax({
@@ -607,9 +608,6 @@ jQuery(document).ready(function () {
                     url: base_url + 'search/ajax_autocomplete',
                     data: {keyword:keyword},
                     dataType: 'HTML',
-                    beforeSend: function(){
-                        element.closest('form').find('.product_search').html('').removeClass('go_in')
-                    },
                     success: function (content) {
                         if(content){
                             element.closest('form').find('.product_search').html(content).addClass('go_in');
