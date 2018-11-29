b72f76c95ddb30061f683c70346691c4fd61ef85
Steven <steven.mucian@gmail.com>
2018-11-30 02:20:27 +0700

    fix cache in debug mode

--- application/core/STEVEN_Controller.php
+++ application/core/STEVEN_Controller.php
@@ -32,7 +32,7 @@ class STEVEN_Controller extends CI_Controller
         $this->_method = $this->router->fetch_method();
 
         //load cache driver
-        if (CACHE_MODE == TRUE) $this->load->driver('cache',array('adapter' => CACHE_ADAPTER, 'backup' => 'file', 'key_prefix' => CACHE_PREFIX_NAME));
+        if (CACHE_MODE == FALSE) $this->load->driver('cache',array('adapter' => CACHE_ADAPTER, 'backup' => 'file', 'key_prefix' => CACHE_PREFIX_NAME));
 
     }
 
@@ -232,7 +232,7 @@ class Admin_Controller extends STEVEN_Controller
                     $controller = $this->router->fetch_class();
                     if (!in_array($controller, array('dashboard'))) {
                         if (!$this->session->admin_permission[$controller]['view']) {//check quyen view
-                            redirect('admin/dashboard/notPermission');
+                            redirect(site_admin_url('dashboard/notPermission'));
                         }
                     }
                 } else {
