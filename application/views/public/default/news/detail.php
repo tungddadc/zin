e71f3fda7603909463a10221d35e47cab1bfe314
Khanh Chu Quang <khanhh3t@gmail.com>
2019-04-18 05:13:43 +0700

    Fix layout detail

--- application/views/public/default/news/detail.php
+++ application/views/public/default/news/detail.php
@@ -2,7 +2,7 @@
   <section class="main-container col2-left-layout">
     <div class="container">
       <div class="row">
-        <div class="col-sm-9 col-sm-push-3">
+        <div class="col-sm-9">
           <article class="col-main">
             <div class="page-title">
               <h2><?php echo !empty($oneCategory) ? $oneCategory->title : '' ?></h2>
