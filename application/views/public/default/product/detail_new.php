d10f861df9c20929d8b6e80a3dd009b2de8bb375
Nguyen Duc Toan <steven.mucian@gmail.com>
2019-07-26 17:34:26 +0700

    fix

--- application/views/public/default/product/detail_new.php
+++ application/views/public/default/product/detail_new.php
@@ -21,21 +21,21 @@ if (!empty($oneItem)):
 
                     <div class="col-md-8 col-sm-7 col-xs-12">
                         <div class="row">
-                            <div class="hidden-xs col-md-3">
+                            <div class="hidden-xs col-md-2" style="padding-right: 0">
                                 <div class="slider-nav">
                                     <?php if (!empty($album)) foreach ($album as $item): ?>
                                         <div>
-                                            <img class="img-responsive" src="<?php echo getImageThumb($item, 768,482,true) ?>" alt="<?php echo getTitle($oneItem) ?>">
+                                            <img width="100%" class="img-responsive" src="<?php echo getImageThumb($item, 140,88,true) ?>" alt="<?php echo getTitle($oneItem) ?>">
                                         </div>
                                     <?php endforeach ?>
                                 </div>
 
                             </div>
-                            <div class="col-xs-12 col-md-9">
+                            <div class="col-xs-12 col-md-10" style="padding-left: 0">
                                 <div class="slider-for">
                                     <?php if (!empty($album)) foreach ($album as $item): ?>
                                         <div>
-                                            <img class="img-responsive" src="<?php echo getImageThumb($item, 768,482, false) ?>" alt="<?php echo getTitle($oneItem) ?>">
+                                            <img style="padding-left: 7px;margin: 0;width: 100%" class="img-responsive" src="<?php echo getImageThumb($item, 768,482, false) ?>" alt="<?php echo getTitle($oneItem) ?>">
                                         </div>
                                     <?php endforeach ?>
                                 </div>
