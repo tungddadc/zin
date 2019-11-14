ec57dd765033310ab2e1211b361ca51c1d03aa9c
Ngoc Pt <ngocptit89@gmail.com>
2019-11-14 16:43:12 +0700

    update accency

--- application/views/public/default/agency/list_agency.php
+++ application/views/public/default/agency/list_agency.php
@@ -1,8 +1,21 @@
-<h2><?php echo (!empty($location))?'DANH SÁCH CỬA HÀNG, ĐẠI LÝ GẦN ĐÂY':'DANH SÁCH CỬA HÀNG, ĐẠI LÝ'; ?></h2>
 <ul>
   <?php if (!empty($data)) foreach ($data as $item): ?>
-    <li>
-      <a href="<?php echo getUrlAgency($item) ?>"><?php echo $item->title ?>, <?php echo $item->address ?> <?php if((!empty($location))): ?><span>(<?php echo round($item->distance,2) ?> km)</span><?php endif; ?></a>
+    <li class="agency-items">
+	    <h3 class="agency-name">
+	        <a href="<?php echo getUrlAgency($item) ?>">
+	            <?php echo $item->title ?>
+	        </a>
+	    </h3>
+	    <div class="agency-info">
+	        <i class="fa fa-map-marker"></i> <?php echo $item->address ?>
+	        <?php if((!empty($location))): ?><span>(<?php echo round($item->distance,2) ?> km)</span><?php endif; ?>
+	    </div>
+	    <div class="agency-info">
+	        <i class="fa fa-clock-o"></i> <?php echo $item->Open_door ?>
+	    </div>
+	    <div class="agency-info">
+	        <i class="fa fa-phone"></i> <?php echo $item->hotline ?>
+	    </div>
     </li>
   <?php endforeach; ?>
 </ul>
