8c306acc6033bee8dbccd48c11fc3ef143c1e862
Ngoc Pt <ngocptit89@gmail.com>
2019-11-14 09:05:23 +0700

    update banner

--- public/js/custom_ngoc.js
+++ public/js/custom_ngoc.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
     });
 
-    $( ".wrap-popup .popup" ).append("<div class='banner-mainmenu banner-menu'><a class='img' href='#'><img alt='Banner menu 1' src='/public/images/banner-menu1.png'></a><a class='img' href='#'><img alt='Banner menu 1' src='/public/images/banner-menu2.png'></a><a class='img' href='#'><img alt='Banner menu 1' src='/public/images/banner-menu3.png'></a></div>");
+    $( ".wrap-popup .popup" ).append("<div class='banner-mainmenu banner-menu'><a class='img' href='#'><img alt='Banner menu 1' src='/public/images/banner-menu1.jpg'></a><a class='img' href='#'><img alt='Banner menu 1' src='/public/images/banner-menu2.jpg'></a><a class='img' href='#'><img alt='Banner menu 1' src='/public/images/banner-menu3.jpg'></a></div>");
 
   
 });
